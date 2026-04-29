902eaba49412f1ae580fa74267939050c867e1bb
rosesharonanchi <rosesharonanchi123@gmail.com>
2026-04-29 16:19:53 +0100

    Made the create-transaction custom hook asynchronous

--- frontend/api/create-transaction.ts
+++ frontend/api/create-transaction.ts
@@ -2,31 +2,27 @@ import { TransactionType } from "@/types/interfaces";
 import axios from "axios";
 
 export interface ResponseType {
-	success: boolean;
-	error: any;
+  success: boolean;
+  error: any;
 }
 
 const BASEURL = "http://localhost:8065";
 
-export const saveTransaction = (payload: TransactionType) => {
-	console.log("Fetch function executed!");
-	let response: ResponseType = {
-		error: null,
-		success: true,
-	};
-	// axios
-	// 	.post(BASEURL + "/api/v1/transactions", payload)
-	// 	.then((res) => {
-	// 		response = {
-	// 			error: undefined,
-	// 			success: true,
-	// 		};
-	// 	})
-	// 	.catch((err) => {
-	// 		response = {
-	// 			error: err,
-	// 			success: false,
-	// 		};
-	// 	});
-	return response;
+export const saveTransaction = async (
+  payload: TransactionType,
+): Promise<ResponseType> => {
+  console.log("Fetch function executed!");
+
+  try {
+    const res = await axios.post(BASEURL + "/api/v1/transactions", payload);
+    return {
+      success: true,
+      error: null,
+    };
+  } catch (err) {
+    return {
+      success: false,
+      error: err,
+    };
+  }
 };
