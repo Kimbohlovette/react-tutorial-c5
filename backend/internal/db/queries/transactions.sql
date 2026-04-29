48682b601b2c915761c79430a93d3dd2fcda32d2
rosesharonanchi <rosesharonanchi123@gmail.com>
2026-04-29 19:03:26 +0100

    Added a WHERE clause in GetTrasactions query that can filter by the type

--- backend/internal/db/queries/transactions.sql
+++ backend/internal/db/queries/transactions.sql
@@ -2,4 +2,6 @@
 INSERT INTO transactions (amount, reason, type) VALUES ($1, $2, $3) RETURNING *;
 
 -- name: GetTransactions :many
-SELECT * FROM transactions;
+SELECT * FROM transactions
+WHERE (CAST(@type_filter AS TEXT) = '' OR type = @type_filter)
+ORDER BY created_at DESC;
